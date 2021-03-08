c2852f0f36e28e1aead98342c9ccf39588e6b1ea
ahcene-kadi <71874119+ahcene-kadi@users.noreply.github.com>
2021-03-08 10:28:43 +0100

    job 4 fini

---
+++ Job04/script.js
@@ -0,0 +1,13 @@
+function bisextile(year) {
+    if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+console.log(bisextile(2024));
+alert(bisextile(2023));
