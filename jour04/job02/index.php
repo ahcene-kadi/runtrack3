534c06601f21e0efc22d306ab9d2904b475f86be
ahcene-kadi <71874119+ahcene-kadi@users.noreply.github.com>
2021-03-11 14:25:30 +0100

    job01 et 02 fini

---
+++ jour04/job02/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>
+    <script  type="text/javascript" src="script.js" async="true"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>job02</title>
+</head>
+<body>
+
+</body>
+</html>
